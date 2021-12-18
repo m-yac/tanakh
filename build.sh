a6a9539dd16843ed713392c9f32876f354a3bfed
Matthew Yacavone <matthew@yacavone.net>
2021-12-17 18:31:03 -0800

    fix github workflow

--- build.sh
+++ build.sh
@@ -24,6 +24,15 @@ cp -R hebcal-es6/dist dist/hebcal-es6/dist
 cp -R hebcal-es6/LICENSE dist/hebcal-es6/LICENSE
 cp -R hebcal-es6/README.md dist/hebcal-es6/README.md
 
+# Build hebcal-leyning and move its outputs
+cd hebcal-leyning
+npm install
+cd ..
+mkdir dist/hebcal-leyning
+cp -R hebcal-leyning/dist dist/hebcal-leyning/dist
+cp -R hebcal-leyning/LICENSE dist/hebcal-leyning/LICENSE
+cp -R hebcal-leyning/README.md dist/hebcal-leyning/README.md
+
 # Build hebcal-leyning's json files for web
 cd hebcal-leyning
 echo "var hebcal__leyning__aliyot =" > dist/aliyot.js
@@ -33,12 +42,3 @@ cat src/holiday-readings.json >> dist/holiday-readings.js
 echo "var hebcal__leyning__numverses =" > dist/numverses.js
 cat src/numverses.json >> dist/numverses.js
 cd ..
-
-# Build hebcal-leyning and move its outputs
-cd hebcal-leyning
-npm install
-cd ..
-mkdir dist/hebcal-leyning
-cp -R hebcal-leyning/dist dist/hebcal-leyning/dist
-cp -R hebcal-leyning/LICENSE dist/hebcal-leyning/LICENSE
-cp -R hebcal-leyning/README.md dist/hebcal-leyning/README.md
